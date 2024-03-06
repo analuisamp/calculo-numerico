66658d0de931f9b5145bdafd80332a7f63562448
JGranvile <joaoggranfer@gmail.com>
2024-03-06 15:07:19 -0300

    Trocando a fonte

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 import pygame
 from button import Button
-from screen import bissecao
+from screens import bissecao, newton, secante
 
 pygame.init()
 
@@ -8,19 +8,23 @@ SCREEN_WIDTH = 1600
 SCREEN_HEIGHT = 900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Projeto 1 - Cálculo numérico')
+pygame.display.set_caption('Projeto 1 - Calculo numerico')
 
 # Carrega imagens
 img_bissecao = pygame.image.load("images/btn_bissecao.png").convert_alpha()
+img_newton = pygame.image.load("images/btn_newton.png").convert_alpha()
+img_secante = pygame.image.load("images/btn_secante.png").convert_alpha()
 img_bissecao = pygame.transform.scale_by(img_bissecao, (1/3 * SCREEN_WIDTH) / img_bissecao.get_width())
+img_newton = pygame.transform.scale_by(img_newton, (1/3 * SCREEN_WIDTH) / img_newton.get_width())
+img_secante = pygame.transform.scale_by(img_secante, (1/3 * SCREEN_WIDTH) / img_secante.get_width())
 
 # Texto
-font = pygame.font.Font('fonts/BebasNeue-Regular.ttf', SCREEN_HEIGHT // 9)
-res_text = font.render('Cálculo Numérico', True, (0, 0, 0), None)
+font = pygame.font.Font('fonts/timesnewroman.ttf', SCREEN_HEIGHT // 9)
+res_text = font.render('Calculo Numerico', True, (0, 0, 0), None)
 res_text_rect = res_text.get_rect()
 res_text_rect.center = (SCREEN_WIDTH / 2, res_text_rect.height)
 
-font_subtitle = pygame.font.Font('fonts/BebasNeue-Regular.ttf', SCREEN_HEIGHT // 15)
+font_subtitle = pygame.font.Font('fonts/timesnewroman.ttf', SCREEN_HEIGHT // 15)
 subtitle = font_subtitle.render('Projeto 1', True, (0, 0, 0), None)
 subtitle_rect = subtitle.get_rect()
 subtitle_rect.center = (SCREEN_WIDTH / 2, res_text_rect.centery + (res_text_rect.height / 1.8))
@@ -29,7 +33,9 @@ center_x = (SCREEN_WIDTH / 2) - (img_bissecao.get_width() / 2)
 first_button_y = (SCREEN_HEIGHT / 6) * 2
 
 # Criando os botões
-btn_bissecao = Button(center_x, first_button_y + img_bissecao.get_height() * 1.1 , img_bissecao)
+btn_bissecao = Button(center_x, first_button_y, img_bissecao)
+btn_secante = Button(center_x, first_button_y + (img_bissecao.get_height() * 1.2 ), img_secante)
+btn_newton = Button(center_x, first_button_y + (img_bissecao.get_height() * 1.2 * 2), img_newton)
 
 def menu():
     run = True
@@ -44,6 +50,12 @@ def menu():
         if btn_bissecao.run(screen):
             print('Bissecao')
             bissecao(screen)
+        elif btn_newton.run(screen):
+            print('Newton')
+            quit_prog = newton(screen)
+        elif btn_secante.run(screen):
+            print('Secante')
+            secante(screen)
 
         if quit_prog:
             run = False
